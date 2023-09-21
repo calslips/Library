8a11a511c17e989c7a232e67529318d7cf36ed13
Caleb Phillips <csphillips5@gmail.com>
2023-09-21 12:49:46 -0400

    implement book dao & service tests

--- src/test/java/BookDAOTest.java
+++ src/test/java/BookDAOTest.java
@@ -1,5 +1,7 @@
-import DAO.PaintingDAO;
-import Model.Painting;
+import DAO.BookDAO;
+import DAO.UserDAO;
+import Model.Book;
+import Model.User;
 import Util.ConnectionSingleton;
 import org.junit.Assert;
 import org.junit.Before;
@@ -8,40 +10,76 @@ import org.junit.Test;
 import java.sql.Connection;
 import java.util.List;
 
-public class PaintingDAOTest {
+public class BookDAOTest {
     Connection conn;
-    PaintingDAO paintingDAO;
+    BookDAO bookDAO;
+    UserDAO userDAO;
     @Before
     public void setUp(){
         conn = ConnectionSingleton.getConnection();
         ConnectionSingleton.resetTestDatabase();
-        paintingDAO = new PaintingDAO(conn);
+        bookDAO = new BookDAO(conn);
+        userDAO = new UserDAO(conn);
     }
+
+    @Test
+    public void testSearchBookByAuthor() {
+        Book testBook = new Book("test author 1", "test title 1");
+        bookDAO.insertBook(testBook);
+        Book actualBook = bookDAO.queryBooksByAuthor("test author 1").get(0);
+        String expectedAuthor = "test author 1";
+        Assert.assertEquals(expectedAuthor, actualBook.getAuthor());
+    }
+
+    @Test
+    public void testSearchBookByTitle() {
+        Book testBook = new Book("test author 2", "test title 2");
+        bookDAO.insertBook(testBook);
+        Book actualBook = bookDAO.queryBooksByTitle("test title 2").get(0);
+        String expectedTitle = "test title 2";
+        Assert.assertEquals(expectedTitle, actualBook.getTitle());
+    }
+
+    @Test
+    public void testSignOutBook() {
+        User user = new User(22, "testname");
+        userDAO.createUser(user);
+
+        Book testBook = new Book(43, "test author", "test title", 22);
+        bookDAO.insertBook(testBook);
+        bookDAO.updateSignedOutBy(testBook.getTitle(), testBook.getAuthor(), testBook.getSignedOutBy(), testBook.getBookId());
+        Book actualBook = bookDAO.queryBooksByAuthor("test author").get(0);
+        int expected = 22;
+        int actual = actualBook.getSignedOutBy();
+        Assert.assertEquals(expected, actual);
+    }
+
+
 
     /**
      * test that all painting that are retrieved are from the expected year.
      */
-    @Test
-    public void testGetByYear(){
+//    @Test
+//    public void testGetByYear(){
 
-        List<Painting> actual = paintingDAO.queryPaintingByYear(1880);
-        int expectedYear = 1880;
-        for(int i = 0 ; i < actual.size() ; i++){
-            Assert.assertEquals(expectedYear, actual.get(i).getYearMade());
-        }
+//        List<Painting> actual = paintingDAO.queryPaintingByYear(1880);
+//        int expectedYear = 1880;
+//        for(int i = 0 ; i < actual.size() ; i++){
+//            Assert.assertEquals(expectedYear, actual.get(i).getYearMade());
+//        }
 
-    }
+//    }
 
     /**
      * test that no paintings that are retrieved are from an unexpected year.
      */
-    @Test
-    public void testGetByYearNotRetrieveUnexpectedYear(){
+//    @Test
+//    public void testGetByYearNotRetrieveUnexpectedYear(){
 
-        List<Painting> actual = paintingDAO.queryPaintingByYear(1880);
-        int expectedYear = 1890;
-        for(int i = 0 ; i < actual.size() ; i++){
-            Assert.assertNotEquals(expectedYear, actual.get(i).getYearMade());
-        }
-    }
+//        List<Painting> actual = paintingDAO.queryPaintingByYear(1880);
+//        int expectedYear = 1890;
+//        for(int i = 0 ; i < actual.size() ; i++){
+//            Assert.assertNotEquals(expectedYear, actual.get(i).getYearMade());
+//        }
+//    }
 }
