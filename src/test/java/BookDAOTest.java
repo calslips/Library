b2ac3edb7f91d024834aa8b0f71ba7d2608da256
Caleb Phillips <csphillips5@gmail.com>
2023-09-26 15:38:20 -0400

    implement bookDAO tests for adding a new book and searching all books

--- src/test/java/BookDAOTest.java
+++ src/test/java/BookDAOTest.java
@@ -2,23 +2,66 @@ import DAO.BookDAO;
 import DAO.UserDAO;
 import Model.Book;
 import Model.User;
+import Service.BookService;
 import Util.ConnectionSingleton;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class BookDAOTest {
     Connection conn;
     BookDAO bookDAO;
+    BookService bookService;
     UserDAO userDAO;
     @Before
     public void setUp(){
         conn = ConnectionSingleton.getConnection();
         ConnectionSingleton.resetTestDatabase();
         bookDAO = new BookDAO(conn);
+        bookService = new BookService(bookDAO);
         userDAO = new UserDAO(conn);
+    }
+
+    /**
+     * Tests inserting a new book into the database
+     */
+    @Test
+    public void testInsertBook() {
+        Book testBook = new Book("test author 1", "test title 1");
+        bookDAO.insertBook(testBook);
+        List<Book> allBooks = bookDAO.queryAllBooks();
+        String expectedAuthor = "test author 1";
+        String expectedTitle = "test title 1";
+        int expectedLength = 1;
+        Assert.assertEquals(expectedAuthor, allBooks.get(0).getAuthor());
+        Assert.assertEquals(expectedTitle, allBooks.get(0).getTitle());
+        Assert.assertEquals(expectedLength, allBooks.size());
+    }
+
+    /**
+     * Tests querying all books in the database
+     */
+    @Test
+    public void testSearchAllBooksEmpty() {
+        List<Book> allBooks = bookDAO.queryAllBooks();
+        int expectedLength = 0;
+        Assert.assertEquals(expectedLength, allBooks.size());
+    }
+
+    /**
+     * Tests querying all books in the database
+     */
+    @Test
+    public void testSearchAllBooksNotEmpty() {
+        bookService.addBook(new Book("author1", "title1"));
+        bookService.addBook(new Book("author2", "title2"));
+        bookService.addBook(new Book("author3", "title3"));
+        List<Book> allBooks = bookDAO.queryAllBooks();
+        int expectedLength = 3;
+        Assert.assertEquals(expectedLength, allBooks.size());
     }
 
     /**
@@ -26,11 +69,11 @@ public class BookDAOTest {
      */
     @Test
     public void testSearchBookByTitleAndAuthor() {
-        Book testBook = new Book("test author 1", "test title 1");
+        Book testBook = new Book("test author 2", "test title 2");
         bookDAO.insertBook(testBook);
-        Book actualBook = bookDAO.queryBooksByTitleAndAuthor("test title 1", "test author 1").get(0);
-        String expectedAuthor = "test author 1";
-        String expectedTitle = "test title 1";
+        Book actualBook = bookDAO.queryBooksByTitleAndAuthor("test title 2", "test author 2").get(0);
+        String expectedAuthor = "test author 2";
+        String expectedTitle = "test title 2";
         Assert.assertEquals(expectedAuthor, actualBook.getAuthor());
         Assert.assertEquals(expectedTitle, actualBook.getTitle());
     }
@@ -40,10 +83,10 @@ public class BookDAOTest {
      */
     @Test
     public void testSearchBookByAuthor() {
-        Book testBook = new Book("test author 1", "test title 1");
+        Book testBook = new Book("test author 3", "test title 3");
         bookDAO.insertBook(testBook);
-        Book actualBook = bookDAO.queryBooksByAuthor("test author 1").get(0);
-        String expectedAuthor = "test author 1";
+        Book actualBook = bookDAO.queryBooksByAuthor("test author 3").get(0);
+        String expectedAuthor = "test author 3";
         Assert.assertEquals(expectedAuthor, actualBook.getAuthor());
     }
 
@@ -52,10 +95,10 @@ public class BookDAOTest {
      */
     @Test
     public void testSearchBookByTitle() {
-        Book testBook = new Book("test author 2", "test title 2");
+        Book testBook = new Book("test author 4", "test title 4");
         bookDAO.insertBook(testBook);
-        Book actualBook = bookDAO.queryBooksByTitle("test title 2").get(0);
-        String expectedTitle = "test title 2";
+        Book actualBook = bookDAO.queryBooksByTitle("test title 4").get(0);
+        String expectedTitle = "test title 4";
         Assert.assertEquals(expectedTitle, actualBook.getTitle());
     }
 
@@ -67,10 +110,10 @@ public class BookDAOTest {
         User user = new User(22, "testname");
         userDAO.createUser(user);
 
-        Book testBook = new Book(43, "test author", "test title", 22);
+        Book testBook = new Book(43, "test author 5", "test title 5", 22);
         bookDAO.insertBook(testBook);
         bookDAO.updateSignedOutBy(testBook, testBook.getSignedOutBy());
-        Book actualBook = bookDAO.queryBooksByAuthor("test author").get(0);
+        Book actualBook = bookDAO.queryBooksByAuthor("test author 5").get(0);
         int expected = 22;
         int actual = actualBook.getSignedOutBy();
         Assert.assertEquals(expected, actual);
@@ -84,10 +127,10 @@ public class BookDAOTest {
         User user = new User(25, "testuser");
         userDAO.createUser(user);
 
-        Book testBook = new Book(45, "test author", "test title", 25);
+        Book testBook = new Book(45, "test author 6", "test title 6", 25);
         bookDAO.insertBook(testBook);
         bookDAO.updateReturnBook(testBook);
-        Book actualBook = bookDAO.queryBooksByAuthor("test author").get(0);
+        Book actualBook = bookDAO.queryBooksByAuthor("test author 6").get(0);
         int expected = 0;
         int actual = actualBook.getSignedOutBy();
         Assert.assertEquals(expected, actual);
