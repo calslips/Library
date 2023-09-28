8fcc9f692a5e54ceb5e16bb1efdbef50ed969779
Caleb Phillips <csphillips5@gmail.com>
2023-09-28 18:11:47 -0400

    implement tests for the various user retrieval methods

--- src/test/java/UserDAOTest.java
+++ src/test/java/UserDAOTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import Service.UserService;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAOTest {
@@ -35,6 +36,81 @@ public class UserDAOTest {
         User user = new User(18, "testname");
         userDAO.createUser(user);
         Assert.assertTrue(userService.checkUser("testname"));
+    }
+
+    /**
+     * Tests that all users can be retrieved from the database.
+     */
+    @Test
+    public void testGetAllUsers() {
+        List<User> users = new ArrayList<>();
+
+        for (int i = 1; i < 6; i++) {
+            User user = new User(i, i + "");
+            users.add(user);
+            userDAO.createUser(user);
+        }
+
+        List<User> retrievedUsers = userDAO.getAllUsers();
+
+        Assert.assertEquals(users.size(), retrievedUsers.size());
+        Assert.assertTrue(retrievedUsers.containsAll(users));
+    }
+
+    /**
+     * Tests that a specified user can be retrieved from the database by their unique user id.
+     */
+    @Test
+    public void testGetUserById() {
+        User userToGet = new User(123321, "retrieveMe");
+        userDAO.createUser(userToGet);
+        Assert.assertEquals(userToGet, userDAO.getUserById(123321));
+    }
+
+    /**
+     * Tests that no user is retrieved from the database when there is no matching user id.
+     */
+    @Test
+    public void testGetNoUserByNonexistentId() {
+        User user = new User(32123, "ignoreMe");
+        userDAO.createUser(user);
+        Assert.assertNull(userDAO.getUserById(90210));
+    }
+
+    /**
+     * Tests that the existence of a user can be validated by their username
+     */
+    @Test
+    public void testUserExistsByUsername() {
+        User user = new User(53489, "iexistthereforeiam");
+        userDAO.createUser(user);
+        Assert.assertTrue(userDAO.userExists("iexistthereforeiam"));
+    }
+
+    /**
+     * Tests that the existence of a user can be invalidated by their username
+     */
+    @Test
+    public void testUserDoesNotExistByUsername() {
+        Assert.assertFalse(userDAO.userExists("whoami"));
+    }
+
+    /**
+     * Tests that the existence of a user can be validated by their id
+     */
+    @Test
+    public void testUserExistsById() {
+        User user = new User(55, "schfiftyfife");
+        userDAO.createUser(user);
+        Assert.assertTrue(userDAO.userExists(55));
+    }
+
+    /**
+     * Tests that the existence of a user can be invalidated by their id
+     */
+    @Test
+    public void testUserDoesNotExistById() {
+        Assert.assertFalse(userDAO.userExists(9000));
     }
 
     @Test
